16dcb950708aedf35995af087fe0cb4b4b534e78
Travis Hunter <travis@travishunter.io>
2023-11-20 18:29:03 -0700

    part 2

--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,63 +1,86 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
-#include <libopencm3/cm3/scb.h>
+#include <libopencm3/cm3/systick.h>
 
-#include "core/system.h"
-#include "core/uart.h"
-#include "timer.h"
+#include <libopencm3/cm3/vector.h> // systick interrupt handler.
+                                   // includes nvic.h which declares sys_tick_handler
+                                
 
-#define BOOTLOADER_SIZE (0x8000U)
+#define LED_PORT (GPIOA)
+#define LED_PIN  (GPIO5)
 
-#define LED_PORT      (GPIOA)
-#define LED_PIN       (GPIO5)
+#define CPU_FREQ     (84000000)
+#define SYSTICK_FREQ (1000)
 
-#define UART_PORT     (GPIOA)
-#define RX_PIN        (GPIO3)
-#define TX_PIN        (GPIO2)
 
-static void vector_setup(void) {
-  SCB_VTOR = BOOTLOADER_SIZE;
-}
+// volatile because only used in sys_tick_handler which the compiler
+// might not see is ever called (it is, but it's an interrupt)
+volatile uint32_t ticks = 0;
 
-static void gpio_setup(void) {
-  rcc_periph_clock_enable(RCC_GPIOA);
-  gpio_mode_setup(LED_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LED_PIN);
-  gpio_set_af(LED_PORT, GPIO_AF1, LED_PIN);
-
-  gpio_mode_setup(UART_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, TX_PIN | RX_PIN);
-  gpio_set_af(UART_PORT, GPIO_AF7, TX_PIN | RX_PIN);
-}
-
-int main(void) {
-  vector_setup();
-  system_setup();
-  gpio_setup();
-  timer_setup();
-  uart_setup();
-
-  uint64_t start_time = system_get_ticks();
-  float duty_cycle = 0.0f;
-
-  timer_pwm_set_duty_cycle(duty_cycle);
-
-  while (1) {
-    if (system_get_ticks() - start_time >= 10) {
-      duty_cycle += 1.0f;
-      if (duty_cycle > 100.0f) {
-        duty_cycle = 0.0f;
-      }
-      timer_pwm_set_duty_cycle(duty_cycle);
-
-      start_time = system_get_ticks();
-    }
-
-    while (uart_data_available()) {
-      uint8_t data = uart_read_byte();
-      uart_write_byte(data + 1);
-    }
-
-    // Do useful work
+// initially defined as a weak function in vector.c. We can redefine
+// it for our purposes since the default is an alias for null_handler()
+void sys_tick_handler(void)
+{
+  ticks++;
+  if(ticks % 100 == 0)
+  {
+    ticks = 0;
+    gpio_toggle(LED_PORT, LED_PIN);
   }
+}
+
+/*
+static uint64_t get_ticks(void)
+{
+  return ticks;
+}
+*/
+
+static void rcc_setup(void)
+{
+  rcc_clock_setup_pll(&rcc_hsi_configs[RCC_CLOCK_3V3_84MHZ]);
+}
+
+static void gpio_setup(void)
+{
+
+  rcc_periph_clock_enable(RCC_GPIOA);
+  gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLDOWN, LED_PIN);
+}
+
+static void systick_setup(void)
+{
+  systick_set_frequency(SYSTICK_FREQ, CPU_FREQ);
+  // need to actually enable
+  systick_counter_enable();
+  // set interrupt
+  // will call sys_tick_handler() when interrupt arrives
+  systick_interrupt_enable();
+}
+
+int main(void)
+{
+begin_main:
+  rcc_setup();
+  gpio_setup();
+  systick_setup();
+
+  //uint64_t start_time = get_ticks();
+  while (1)
+  {
+    /*
+    if(get_ticks() - start_time > 1000)
+    {
+      gpio_toggle(LED_PORT, LED_PIN);
+      start_time = get_ticks();
+    }
+    */
+    // do useful work
+  }
+
+// If for some reason we broke out of the while loop,
+// go back to beginning of main().
+goto begin_main;
 
   // Never return
   return 0;

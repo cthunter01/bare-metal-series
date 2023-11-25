37422ba3bddf6d93c72b62af2722bdaed68a2fbd
Travis Hunter <travis@travishunter.io>
2023-11-25 07:05:47 -0700

    WIP

--- bootloader/src/bootloader.cpp
+++ bootloader/src/bootloader.cpp
@@ -2,24 +2,33 @@
 
 #include <libopencm3/stm32/memorymap.h>
 #include <libopencm3/cm3/scb.h>
+#include <libopencm3/cm3/vector.h>
 
 #define BOOTLOADER_SIZE (0x8000U) // 32K
 #define MAIN_APP_START_ADDR (FLASH_BASE + BOOTLOADER_SIZE)
 
 // This will jump to the entrypoint of 
-void jump_to_application_main()
+static void jump_to_application_main()
 {
-  uint32_t* reset_vector_entry = reinterpret_cast<uint32_t*>(MAIN_APP_START_ADDR + 4U);
-  // address of reset_vector is stored in the reset_vector_entry
-  uint32_t* reset_vector = reinterpret_cast<uint32_t*>(*reset_vector_entry);
-
-  void (*fcnPtr)(void) = reinterpret_cast<void (*)(void)>(reset_vector);
+  /*
+  typedef void (*void_fn)(void);
+  uint32_t* app_vector_table = reinterpret_cast<uint32_t*>(MAIN_APP_START_ADDR);
+  void_fn fcnPtr = reinterpret_cast<void_fn>(app_vector_table[1]);
 
   // maybe reset stack pointer here?
 
   // Reset vector table and call application
   SCB_VTOR = BOOTLOADER_SIZE;
   fcnPtr();
+  SCB_VTOR = 0x0;
+  */
+
+  // Start of application is the vector table
+  vector_table_t* main_vector_table = reinterpret_cast<vector_table_t*>(MAIN_APP_START_ADDR);
+
+  // Reset the vector table register to point to the new vector table
+  SCB_VTOR = BOOTLOADER_SIZE;
+  main_vector_table->reset();
 }
 int main()
 {
